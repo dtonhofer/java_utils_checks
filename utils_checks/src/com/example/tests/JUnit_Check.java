701be9a41a00b78876a50edc95d74b95bc34eda6
David Tonhofer <ronerycoder@gluino.name>
2014-02-24 10:02:37 +0100

    Suppressed some warnings

--- utils_checks/src/com/example/tests/JUnit_Check.java
+++ utils_checks/src/com/example/tests/JUnit_Check.java
@@ -1,7 +1,5 @@
 package com.example.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
